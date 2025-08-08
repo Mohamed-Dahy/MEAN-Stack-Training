238fef008ce59199a4c7acfb54067db92af6eb75
Mohamed-Dahy <163175166+Mohamed-Dahy@users.noreply.github.com>
2025-08-08 16:40:42 +0300

    Update server.js

--- Final Project/server.js
+++ Final Project/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const eventRouter = require("./Routes/events.routes");
 const usersRouter = require("./Routes/users.routes");
+const adminRouter = require("./Routes/admin.routes");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -13,6 +15,9 @@ connectDB();
 
 app.use("/Eventora", eventRouter);
 app.use("/Eventora", usersRouter);
+app.use("/Eventora", adminRouter);
+
+app.use("/uploads",express.static(path.join(__dirname, "uploads")));
 
 app.listen(PORT, () => {
   console.log(`My Events Server is Listening on port ${PORT}`);
