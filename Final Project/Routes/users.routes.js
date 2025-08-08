57c01eada9be1d56b5a1f35822c71e30b9be8148
Mohamed-Dahy <163175166+Mohamed-Dahy@users.noreply.github.com>
2025-08-08 16:41:27 +0300

    Update users.routes.js

--- Final Project/Routes/users.routes.js
+++ Final Project/Routes/users.routes.js
@@ -1,11 +1,23 @@
 const userscontroller = require("../Controllers/users.controllers");
 const express = require("express");
+const upload = require("../middlewares/uploads.middlewares");
+const error_multer = require("../middlewares/multererrors");
+
 const router = express.Router();
-router.get("/users", userscontroller.getAllUsers);
-router.post("/signup", userscontroller.signup);
-router.post("/login", userscontroller.login);
-router.post("/favoriteEvents",userscontroller.protectRoutes, userscontroller.addToFavEvents);
-router.delete("/favorite/removeEvent", userscontroller.protectRoutes,userscontroller.removeFromFavEvents);
+
+
+router.post("/signup", upload.single("photo"),error_multer,userscontroller.signup); //worked
+
+
+router.get("/login", userscontroller.login);//worked    
+router.post("/userprofile/addtofavoriteEvents",userscontroller.protectRoutes, userscontroller.addToFavEvents);//worked
+router.delete("/userprofile/favorite/removeEvent", userscontroller.protectRoutes,userscontroller.removeFromFavEvents);//worked
+router.post("/userprofile/bookevent", userscontroller.protectRoutes, userscontroller.bookevent);// worked
+router.delete("/userprofile/cancelbooking", userscontroller.protectRoutes, userscontroller.cancelbooking);//worked
+router.post("/userprofile/createEvent", userscontroller.protectRoutes, userscontroller.createEvent);//worked
+router.patch("/userprofile/updateEvent/:id", userscontroller.protectRoutes, userscontroller.updatemyEvent); //worked
+router.delete("/userprofile/deleteEvent/:id", userscontroller.protectRoutes, userscontroller.deletemyEvent); //worked
+
 
 
 
