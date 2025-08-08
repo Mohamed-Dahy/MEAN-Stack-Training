64c2578fb8c79b08c9c817da161ced145fa53dcc
Mohamed-Dahy <163175166+Mohamed-Dahy@users.noreply.github.com>
2025-08-08 16:44:04 +0300

    Update events.conrollers.js

--- Final Project/Controllers/events.conrollers.js
+++ Final Project/Controllers/events.conrollers.js
@@ -1,18 +1,6 @@
 const Event = require("../Models/events.model");
 const mongoose = require("mongoose");
 
-const createEvent = async (req, res) => {
-  try {
-    const newEvent = await Event.create(req.body);
-    res.status(201).json({
-      status: "Success",
-      data: { game: newEvent },
-    });
-  } catch (error) {
-    res.status(400).json({ status: "Fail", message: error.message });
-  }
-};
-
 const getAllEvents = async (req, res) => {
   try {
     const events = await Event.find();
@@ -46,47 +34,32 @@ const getEventById = async (req, res) => {
   }
 };
 
-const updateEvent = async (req, res) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).json({ status: "Fail", message: "Invalid ID" });
+const searchbycategory = async (req,res)=>{
+try{
+  const {category} = req.body;
+  if (!category) {
+    return res.status(400).json({ status: "Fail", message: "Category is required" });
   }
-
-  try {
-    const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    res.status(200).json({
-      status: "Success",
-      data: { event: updatedEvent },
-    });
-  } catch (error) {
-    res.status(500).json({ status: "Fail", message: error.message });
+  const events = await Event.find({ category: category });
+  if (events.length === 0) {
+    return res.status(404).json({ status: "Fail", message: "No events found in this category" });
   }
+  res.status(200).json({
+    status: "Success",
+    length: events.length,
+    data: { events },
+  });
+
+}catch(error){
+  res.status(500).json({ status: "Fail", message: error.message });
+}
 };
 
-const deleteEvent = async (req, res) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).json({ status: "Fail", message: "Invalid ID" });
-  }
-
-  try {
-    const deletedEvent = await Game.findByIdAndDelete(req.params.id);
-
-    res.status(200).json({
-      status: "Success - Deleted",
-      data: { event: deletedEvent },
-    });
-  } catch (error) {
-    res.status(500).json({ status: "Fail", message: error.message });
-  }
-};
 
 module.exports = {
-  createEvent,
+
   getAllEvents,
   getEventById,
-  updateEvent,
-  deleteEvent
+  searchbycategory
+
 };
