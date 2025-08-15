c31fa40b5b29ad959fa9c91a5a023a88f1b8e7cb
Mohamed-Dahy <163175166+Mohamed-Dahy@users.noreply.github.com>
2025-08-15 23:45:50 +0300

    Update user-service.ts

--- Final Project/Frontend/src/app/services/user-service.ts
+++ Final Project/Frontend/src/app/services/user-service.ts
@@ -2,11 +2,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { exhaustMap, map, Observable, take } from 'rxjs';
 import { AuthService } from './auth-service';
+import { EventModel } from '../models/eventModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
   private http = inject(HttpClient);
   private authservice = inject(AuthService)
   private URL = 'http://localhost:5000/Eventora/userprofile'
@@ -18,7 +20,9 @@ export class UserService {
         const headers = new HttpHeaders({
           Authorization: `Bearer ${user?.token}`,
         });
-
+        if (!user) {
+          throw new Error('User not authenticated');
+        }
         return this.http
           .post<any>(`${this.URL}/addtofavoriteEvents`, { eventId }, { headers })
           .pipe(map((response) => response.data.favEvents));
@@ -43,7 +47,7 @@ export class UserService {
     );
   }
 
-  // servi ece to book an event
+  // service to book an event
 
   bookevent(Eventid: string): Observable<any> {
     return this.authservice.user.pipe(
@@ -54,10 +58,78 @@ export class UserService {
         });
 
         return this.http
-          .post<any>(`${this.URL}/bookevent/${Eventid}`, { headers })
+          .post<any>(`${this.URL}/bookevent/${Eventid}`, {},{ headers })
           .pipe(map((response) => response.data.ticket));
       })
     );
   }
+
+
+  cancelbooking(ticketid:string): Observable<any> {
+    return this.authservice.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${user?.token}`,
+        });
+
+        return this.http
+          .delete<any>(`${this.URL}/cancelbooking/${ticketid}`, { headers: headers })
+          .pipe(map((response) => response.data.ticket));
+      })
+    );
+  }
+
+  createEvent(event: any): Observable<any> {
+    return this.authservice.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${user?.token}`,
+        });
+
+        return this.http
+          .post<any>(`${this.URL}/createEvent`, event, { headers })
+          .pipe(map((response) => response.data.event));
+      })
+    );
+  }
+
+  updateEvent(id: string , updateddata : Partial<EventModel>): Observable<any> {
+    return this.authservice.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${user?.token}`,
+        });
+
+        if (!user) {
+          throw new Error('User not authenticated');
+        }
+
+        return this.http
+          .patch<any>(`${this.URL}/updateEvent/${id}`, updateddata, { headers })
+          .pipe(map((response) => response.data.event));
+      })
+    );}
+
+    deleteEvent(id:string): Observable<any> {
+      return this.authservice.user.pipe(
+        take(1),
+        exhaustMap((user) => {
+          const headers = new HttpHeaders({
+            Authorization: `Bearer ${user?.token}`,
+          });
+
+          if (!user) {
+            throw new Error('User not authenticated');
+          }
+
+          return this.http
+            .delete<any>(`${this.URL}/deleteEvent/${id}`, { headers })
+            .pipe(map((response) => response.data.event));
+        })
+      );
+    }
 }
 
