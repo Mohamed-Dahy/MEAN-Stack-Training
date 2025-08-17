926d813a7f52c58c27ccc43092bf376e24791375
Mohamed-Dahy <163175166+Mohamed-Dahy@users.noreply.github.com>
2025-08-17 16:01:32 +0300

    Update app.ts

--- Final Project/Frontend/src/app/app.ts
+++ Final Project/Frontend/src/app/app.ts
@@ -9,10 +9,15 @@ import {Login} from './login/login'
 import { UserProfile } from './user-profile/user-proflie';
 import { AuthService } from './services/auth-service';
 import { AdminProfile } from './admin-profile/admin-profile';
+import { Signup } from './signup/signup';
+import { Navbar } from './navbar/navbar';
+import { Sidebar } from './sidebar/sidebar';
+import { Menu } from './menu/menu';
+
 
 @Component({
   selector: 'app-root',
-  imports: [ CommonModule, FormsModule, EventList,Login, UserProfile  , AdminProfile ],
+  imports: [ CommonModule, FormsModule, EventList,Login, UserProfile  , AdminProfile , Signup, Navbar,Sidebar,Menu] ,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
