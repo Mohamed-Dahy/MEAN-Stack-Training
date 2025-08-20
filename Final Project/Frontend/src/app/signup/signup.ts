aecee51d2be17c433db6729e7af8f55d75ef5052
Mohamed-Dahy <163175166+Mohamed-Dahy@users.noreply.github.com>
2025-08-20 15:56:30 +0300

    Add files via upload

--- Final Project/Frontend/src/app/signup/signup.ts
+++ Final Project/Frontend/src/app/signup/signup.ts
@@ -1,6 +1,8 @@
 import { Component, inject, ViewChild } from "@angular/core";
 import { FormsModule, NgForm } from "@angular/forms";
 import { AuthService } from "../services/auth-service";
+import { Router } from "@angular/router";
+import { CanComponentDeactivate } from "../models/can-component-deactivate.interface";
 
 @Component({
   selector: "app-signup",
@@ -8,44 +10,49 @@ import { AuthService } from "../services/auth-service";
   templateUrl: "./signup.html",
   styleUrl: "./signup.css",
 })
-export class Signup {
+export class Signup implements CanComponentDeactivate {
+  private router = inject(Router);
 
- loading = false;
+  loading = false;
   serverError = "";
   serverSuccess = "";
   selectedFile: File | null = null;
   selectedFileName = "";
   passwordMismatch = false;
+  hasUnsavedChanges = false;
 
   private authService = inject(AuthService);
   @ViewChild("signUpForm") signUpForm!: NgForm;
 
+
+  onInputChange() {
+    if (this.signUpForm?.dirty) {
+      this.hasUnsavedChanges = true;
+    }
+  }
+
   onFileSelected(e: Event) {
-    console.log(e);
-
     const input = e.target as HTMLInputElement;
-
     this.selectedFile = input.files?.[0] || null;
     this.selectedFileName = this.selectedFile?.name || "";
+    this.hasUnsavedChanges = true; // file selection = change
   }
 
   onSubmit() {
-    console.log("server is working")
     if (this.signUpForm.invalid) return;
 
-    const { Firstname, Lastname, username, email, password, confirmpassword } = this.signUpForm.value;
+    const { Firstname, Lastname, username, email, password, confirmpassword } =
+      this.signUpForm.value;
     this.passwordMismatch = password !== confirmpassword;
     if (this.passwordMismatch) return;
 
     const fd = new FormData();
-    fd.append("Firstname",Firstname)
-    fd.append("Lastname",Lastname)
+    fd.append("Firstname", Firstname);
+    fd.append("Lastname", Lastname);
     fd.append("username", username);
     fd.append("email", email);
     fd.append("password", password);
-    console.log(password);
     fd.append("confirmpassword", confirmpassword);
-    console.log(confirmpassword) // should handle in front instead of back
     if (this.selectedFile) fd.append("photo", this.selectedFile);
 
     this.loading = true;
@@ -54,17 +61,19 @@ export class Signup {
 
     this.authService.signup(fd).subscribe({
       next: (token) => {
-        console.log(token);
         this.loading = false;
         this.serverSuccess = "Account created successfully";
         this.signUpForm.reset();
         this.selectedFile = null;
         this.selectedFileName = "";
+        this.hasUnsavedChanges = false; 
+        this.router.navigate(["/menu"]).then(() => {
+          window.location.reload();
+        });
       },
       error: (error) => {
         this.loading = false;
-        this.serverError = "error.message;"
-        console.log(this.serverError)
+        this.serverError = error.message || "Signup failed";
       },
     });
   }
