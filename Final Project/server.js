d5a6bc9a8d3ff2e60e489892db25b388b7b3f3ef
Mohamed-Dahy <163175166+Mohamed-Dahy@users.noreply.github.com>
2025-08-20 16:01:57 +0300

    Add files via upload

--- Final Project/server.js
+++ Final Project/server.js
@@ -4,8 +4,10 @@ const usersRouter = require("./Routes/users.routes");
 const adminRouter = require("./Routes/admin.routes");
 const connectDB = require("./config/db");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+app.use(cors({origin: "*"}));
 app.use(express.json());
 
 require("dotenv").config();
@@ -17,7 +19,9 @@ app.use("/Eventora", eventRouter);
 app.use("/Eventora", usersRouter);
 app.use("/Eventora", adminRouter);
 
-app.use("/uploads",express.static(path.join(__dirname, "uploads")));
+// Serve static files for user and movie uploads
+app.use("/uploads/users", express.static(path.join(__dirname, "uploads/users")));
+app.use("/uploads/events", express.static(path.join(__dirname, "uploads/events")));
 
 app.listen(PORT, () => {
   console.log(`My Events Server is Listening on port ${PORT}`);
